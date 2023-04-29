e2324c338d3b1a460b472a4b196c59a47d036488
Nyxfor13days <nyxfor13days@gmail.com>
2023-04-30 03:33:14 +0530

    Create CONSTANTS.ts

---
+++ src/scripts/CONSTANTS.ts
@@ -0,0 +1,6 @@
+export const NAV_ITEMS = [
+  { label: "Home", path: "/" },
+  { label: "About", path: "/about" },
+  { label: "Blog", path: "/blog" },
+  { label: "Contact", path: "/contact" },
+];
